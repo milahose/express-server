d5e4f17aea0e6f9475d0a46c0d64f3ff4aa0130f
Mila Hose <mila.hose@gmail.com>
2019-10-28 01:33:45 -0700

    Fix file name

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const blogPostsRouter = require("./blogRouter");
+const blogRouter = require("./blogRouter");
 
 app.use(express.json());
 
@@ -16,7 +16,7 @@ app.get('/portfolio', (req, res) => {
   res.send('Welcome to the portfolio page!');
 })
 
-app.use('/blog', blogPostsRouter);
+app.use('/blog', blogRouter);
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
